ba8da20bab69e0700c29d434d0e91c100cb063ad
Ghesker-afk <spacexz12345@gmail.com>
2025-09-15 12:01:49 -0300

    feat: created schema and model folders and logic to connect to MongoDB.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
-// async errors
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -14,20 +17,24 @@ app.use(express.json());
 // routes
 
 app.get('/', (req, res) => {
-  res.send('<h1>Store API</h1><a href="/api/v1/products">products router</a>"');
+  res.send('<h1>Store API</h1>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 // products route
+
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const start = async() => {
+const start = async () => {
   try {
-    // connect DB
-    app.listen(port, console.log(`Server is listening on port: ${port}`));
+    // connect to DB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port: ${port}...`));
   } catch (error) {
     console.log(error);
   }

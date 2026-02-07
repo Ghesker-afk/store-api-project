1dfe7bd2d55a976b51e035354827791ec5ae3c56
Ghesker-afk <spacexz12345@gmail.com>
2026-02-07 18:40:29 -0300

    feat: start using TypeScript and creation of a env file to be more organised

---
+++ src/constants/env.ts
@@ -0,0 +1,13 @@
+const getEnv = (key: string, defaultValue?: string): string => {
+  const value = process.env[key] || defaultValue;
+
+  if (value === undefined) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+
+  return value;
+};
+
+export const MONGO_URI = getEnv("MONGO_URI");
+export const NODE_ENV = getEnv("NODE_ENV");
+export const PORT = getEnv("PORT", "4004");
